c4244864d8e6bb97bf8e5f4dae0cc578cd06dac1
Wai-Lau <wailunlau@hotmail.com>
2016-01-24 22:03:07 -0500

    Let's get this started
    Stole some code and other things :P

---
+++ TextReceiver.py
@@ -0,0 +1,38 @@
+import json
+#the next two are just for here
+import pygame
+import time
+import speech_recognition as sr
+
+# obtain audio from the microphone
+r = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Say something!")
+    audio = r.listen(source)
+print("done listening")
+
+# recognize speech using Google Speech Recognition
+try:
+    # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+    # otherwise `r.recognize_google(audio)`
+	phrase = r.recognize_google(audio)
+	words = phrase.split(" ")
+	for word in words:
+		print("-"+word)
+except sr.UnknownValueError:
+    print("Google Speech Recognition could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+#this part for receiving JSON
+'''
+receivedJSON = open('textReceived.txt', 'r')
+receivedText = json.loads(receivedJSON.read())
+listOfWords = receivedText['QueryResult']['mrec_results']['transcriptions']
+print(listOfWords[0])
+'''
+
+pygame.mixer.init(frequency=22050, size=-16, channels=2, buffer=4096)
+pygame.mixer.music.load("WayUp.wav")
+pygame.mixer.music.play()
+time.sleep(30)
